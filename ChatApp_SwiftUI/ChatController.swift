52efa6ec4edd1e3292600ca07b8a589868d85fa1
Rahul Gupta <rahul@Rahuls-Mac-mini.local>
2019-11-24 20:55:53 +0530

    Completed

--- ChatApp_SwiftUI/ChatController.swift
+++ ChatApp_SwiftUI/ChatController.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2019 Rahul Gupta. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+import Combine
+
+class ChatController: ObservableObject {
+    
+    var didChange = PassthroughSubject<Void,Never>()
+    
+    @Published var messages : [ChatMessage] = [
+        ChatMessage.init(message: "Hi Rahul", avator: "A", color: .red),
+        ChatMessage.init(message: "Hi Naval", avator: "B", color: .blue)
+    ]
+    
+    func sendMessage(_ chatMsg : ChatMessage) {
+        messages.append(chatMsg)
+        didChange.send(())
+    }
+}
